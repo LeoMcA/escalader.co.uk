15b679136187b45a72ab1a6d15cbe0fdbfa747f1
Leo McArdle <leomcardle@gmail.com>
2013-11-30 11:46:19 +0000

    initial commit

---
+++ js/script.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+
+  $('.box').css({'display': 'none'});
+
+  $('.box:first').css({'display': 'block', 'position': 'fixed'});
+
+  var lidMoveHeight = $('.row-box:eq(1)').offset().top - $('.row-box:first').offset().top - 75;
+  var lidStopHeight = $('.row-box:eq(2)').offset().top - $('.row-box:first').offset().top - 75;
+
+  $(window).scroll(function(){
+        console.log($(window).scrollTop(), lidMoveHeight, lidStopHeight)
+    if($(window).scrollTop() <= lidMoveHeight) {
+      $('.box:first').css({'position': 'fixed', 'top': ''});
+      $('.box:first .box-ribbon').css({'display': '', 'opacity': 1 - $(window).scrollTop() / (lidMoveHeight - 20)});
+      $('.box:first .box-lid').css({'left': 57});
+    } else if($(window).scrollTop() <= lidStopHeight) {
+      $('.box:first').css({'position': 'fixed', 'top': ''});
+      $('.box:first .box-ribbon').css({'display': 'none'});
+      $('.box:first .box-lid').css({'display': '', 'left': 57 + 2 * ($(window).scrollTop() - lidMoveHeight)});
+    } else {
+      $('.box:first .box-lid').css({'display': 'none'});
+      $('.box:first').css({'position': 'relative', 'top': lidStopHeight});
+    }
+  });
+
+});
