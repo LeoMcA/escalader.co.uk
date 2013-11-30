51d15ee59d3dae333fe6ead0ff2d2769bd68428e
Leo McArdle <leomcardle@gmail.com>
2013-11-30 12:30:50 +0000

    good enough for christmas fayre

--- js/script.js
+++ js/script.js
@@ -1,26 +1,38 @@
 $(document).ready(function(){
 
-  $('.box').css({'display': 'none'});
+  var theBestFunction = function(){
+    if($(window).width() < 980){
 
-  $('.box:first').css({'display': 'block', 'position': 'fixed'});
+      $(window).off('scroll');
 
-  var lidMoveHeight = $('.row-box:eq(1)').offset().top - $('.row-box:first').offset().top - 75;
-  var lidStopHeight = $('.row-box:eq(2)').offset().top - $('.row-box:first').offset().top - 75;
+      $('.box').css({'display': ''});
+      $('.box:first').css({'display': '', 'position': ''});
 
-  $(window).scroll(function(){
-        console.log($(window).scrollTop(), lidMoveHeight, lidStopHeight)
-    if($(window).scrollTop() <= lidMoveHeight) {
-      $('.box:first').css({'position': 'fixed', 'top': ''});
-      $('.box:first .box-ribbon').css({'display': '', 'opacity': 1 - $(window).scrollTop() / (lidMoveHeight - 20)});
-      $('.box:first .box-lid').css({'left': 57});
-    } else if($(window).scrollTop() <= lidStopHeight) {
-      $('.box:first').css({'position': 'fixed', 'top': ''});
-      $('.box:first .box-ribbon').css({'display': 'none'});
-      $('.box:first .box-lid').css({'display': '', 'left': 57 + 2 * ($(window).scrollTop() - lidMoveHeight)});
     } else {
-      $('.box:first .box-lid').css({'display': 'none'});
-      $('.box:first').css({'position': 'relative', 'top': lidStopHeight});
+      $('.box').css({'display': 'none'});
+      $('.box:first').css({'display': 'block', 'position': 'fixed'});
+      var lidMoveHeight = $('.row-box:eq(1)').offset().top - $('.row-box:first').offset().top - 75;
+      var lidStopHeight = $('.row-box:eq(2)').offset().top - $('.row-box:first').offset().top - 75;
+
+      $(window).off('scroll');
+      $(window).on('scroll', function(){
+        if($(window).scrollTop() <= lidMoveHeight) {
+          $('.box:first').css({'position': 'fixed', 'top': ''});
+          $('.box:first .box-ribbon').css({'display': '', 'opacity': 1 - $(window).scrollTop() / (lidMoveHeight - 20)});
+          $('.box:first .box-lid').css({'left': 57});
+        } else if($(window).scrollTop() <= lidStopHeight) {
+          $('.box:first').css({'position': 'fixed', 'top': ''});
+          $('.box:first .box-ribbon').css({'display': 'none'});
+          $('.box:first .box-lid').css({'display': '', 'left': 57 + 2 * ($(window).scrollTop() - lidMoveHeight)});
+        } else {
+          $('.box:first .box-lid').css({'display': 'none'});
+          $('.box:first').css({'position': 'relative', 'top': lidStopHeight});
+        }
+      });
     }
-  });
+  }
+
+  theBestFunction();
+  $(window).resize(theBestFunction);
 
 });
