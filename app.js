ca58877b733a7e16261486e50d305c270e94ac2d
Leo McArdle <leomcardle@gmail.com>
2014-01-30 21:47:10 +0000

    switched to swig templating system

--- app.js
+++ app.js
@@ -7,13 +7,17 @@ var express = require('express');
 var routes = require('./routes');
 var http = require('http');
 var path = require('path');
+var swig = require('swig');
 
 var app = express();
+
+// set up swig
+app.engine('html', swig.renderFile);
+app.set('view engine', 'html');
 
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.json());
@@ -32,6 +36,8 @@ require("express-persona")(app, {
 // development only
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
+  app.set('view cache', false);
+  swig.setDefaults({ cache: false });
 }
 
 app.get('/', routes.index);
